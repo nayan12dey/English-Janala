50bd4ea7d47bdd20bcd1c603d12874abf2e730f7
Nayan Dey <ndeynayan123@gmail.com>
2026-03-03 23:48:52 +0530

    add word pronunciation audio feature

--- script/index.js
+++ script/index.js
@@ -97,7 +97,7 @@ const displayLevelWord = (words) => {
     wordContainer.innerHTML = "";
 
     // handle error for length is zero
-    if(words.length == 0){
+    if (words.length == 0) {
         wordContainer.innerHTML = `
         <div class="text-center col-span-full rounded-xl py-10 space-y-2">
         <img src="./assets/alert-error.png" class="mx-auto" />
@@ -122,10 +122,10 @@ const displayLevelWord = (words) => {
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4 transition-all duration-300 hover:shadow-lg hover:shadow-sky-300">
             <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="font-semibold">Meaning / Pronounciation</p>
-            <div class="font-bangla text-2xl font-medium">${word.meaning? word.meaning : "অর্থ পাওয়া যায়নি" } / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}</div>
+            <div class="font-bangla text-2xl font-medium">${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF30] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF30] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF30] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
              
@@ -143,11 +143,11 @@ const displayLevelWord = (words) => {
 // Feature 3: loadWordDetail and displayWordDetail in the Modal ******
 
 /* load word detail in Modal */
-const loadWordDetail = async(id) => {
+const loadWordDetail = async (id) => {
     const url = `https://openapi.programming-hero.com/api/word/${id}`;
     console.log(url);
     const response = await fetch(url);
-    const details =  await response.json();
+    const details = await response.json();
     displayWordDetails(details.data);
 
 }
@@ -180,7 +180,7 @@ const displayWordDetails = (word) => {
     // Generate synonyms HTML
     const synonymsHTML = word.synonyms && word.synonyms.length ? word.synonyms.map(elem => `<span class="btn mr-2 mb-2">${elem}</span>`).join("") : `<span class="text-gray-500">No synonyms found</span>`;
 
-    
+
     detailsBox.innerHTML = `
     <div id="details-container" class="space-y-4">
                 <div class="">
@@ -188,7 +188,7 @@ const displayWordDetails = (word) => {
                 </div>
                 <div class="">
                     <h2 class="font-bold mb-2">Meaning</h2>
-                    <p class="font-bangla font-semibold">${word.meaning? word.meaning : `<span class="text-gray-500 font-normal">No Meaning found</span>`}</p>
+                    <p class="font-bangla font-semibold">${word.meaning ? word.meaning : `<span class="text-gray-500 font-normal">No Meaning found</span>`}</p>
                 </div>
                 <div class="">
                     <h2 class="font-bold mb-2">Example</h2>
@@ -214,11 +214,11 @@ const displayWordDetails = (word) => {
 
 /* spinner manage */
 const manageSpinner = (status) => {
-    if(status == true){
+    if (status == true) {
         document.getElementById("spinner").classList.remove("hidden")
         document.getElementById("word-container").classList.add("hidden");
     }
-    else{
+    else {
         document.getElementById("word-container").classList.remove("hidden");
         document.getElementById("spinner").classList.add("hidden")
     }
@@ -233,26 +233,25 @@ document.getElementById("btn-search").addEventListener("click", () => {
     console.log(searchValue);
 
     fetch("https://openapi.programming-hero.com/api/words/all")
-    .then(response => response.json())
-    .then(data => {
-        const allWords = data.data;
-        console.log(allWords)
-        const filterWords = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
-        // console.log(filterWords);
-        displayLevelWord(filterWords);
-    });
+        .then(response => response.json())
+        .then(data => {
+            const allWords = data.data;
+            console.log(allWords)
+            const filterWords = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+            // console.log(filterWords);
+            displayLevelWord(filterWords);
+        });
 })
 
 
+// Feature 6: Words Sound  ******
+function pronounceWord(word) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
+}
 
 
-/* create synonyms in modal */
-// const createElements = (arr) => {
-//     // console.log(arr);
-
-//     const htmlElements = arr.map(elem => `<span class="btn">${elem}</span>`)
-//     return htmlElements.join(" ");
-// }
 
 
 
